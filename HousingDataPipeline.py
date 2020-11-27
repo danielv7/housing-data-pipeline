08fa81099eafe60dcff28a690d73f792157d0651
Daniel <daniel.a.vaughan@biola.edu>
2020-11-27 01:06:53 -0800

    Update HousingDataPipeline.py

--- HousingDataPipeline.py
+++ HousingDataPipeline.py
@@ -2,27 +2,50 @@ import pandas as pd
 import psycopg2
 
 def main():
+    print("Press 1 To Create Source and Destination Datatables")
+    print("Press 2 Inserting Inital CSV file Into Source Datatable")
+    print("Press 3 Inserting New Net Listings Into Destination Datatable")
+    print("Press 4 Inserting All Updated Tracked Listing's Into Destination Datatable")
+    print("Press 5 To Exit Program")
 
-  #createSourceTable()
-  #createDestinationTable()
-
-  #readingInitialSourceCSVFile()
-  #settingUpInitialDataInsert()
-
-  #readingInitialSourceCSVFile()
-  #readingNextIncrementalLoadCSVFile()
-  #findingNewNetListing()
-
-  readingInitialSourceCSVFile()
-  readingNextIncrementalLoadCSVFile()
-  findingTrackedVariableChanged()
+    loop=True      
+    while loop:         
+      choice = int(input("Enter your choice [1-5]: "))
+     
+      if choice==1:     
+        print("Option 1 Selected")
+        #STEP 1 create source and destination datatables
+        createSourceTable()
+        createDestinationTable()
+      elif choice==2:
+        print("Option 2 Selected")
+        #STEP 2 Inserting inital CSV filve into source source datatable
+        readingInitialSourceCSVFile()
+        settingUpInitialDataInsert()
+      elif choice==3:
+        print("Option 3 Selected")
+        #STEP 3 Inserting new net listings into destination datatable
+        readingInitialSourceCSVFile()
+        readingNextIncrementalLoadCSVFile()
+        findingNewNetListing()
+      elif choice==4:
+        print("Option 4 Selected")
+        #STEP 4 reading new CSV and Inserting all updated tracked row's into destination datatable
+        readingNextIncrementalLoadCSVFile()
+        retreavingPossibleUpdatedListings()
+      elif choice==5:
+        print("Option 5 Selected Ending Program")
+        loop=False # This will make the while loop to end as not value of loop is set to False
+      else:
+        # Any integer inputs other than values 1-5 we print an error message
+        loop=False
 
 
 def settingCSVFilePath():
   global CSVFile 
   CSVFile  = "/Users/danielvaughan/Desktop/Python/sample_data.csv"
 
-#Reading from source data csv, 1000 records at a time & Creating DF SourceData
+#Reading from source data csv, 1000 records at a time & creating DataFrame SourceData
 def readingInitialSourceCSVFile():
 
   global sourceData
@@ -33,17 +56,16 @@ def readingInitialSourceCSVFile():
   #Replace NaN with None
   sourceData = sourceData.where(pd.notnull(sourceData), None)
 
-#Reading Next Incremental Load from CSV of 1000 or less & creating DF UpdatedData
+#Reading Next Incremental Load from CSV off 1000 or less & creating DataFrame UpdatedData
 def readingNextIncrementalLoadCSVFile():
   global updatedData
   batchs = []
-  for batch in pd.read_csv('/Users/danielvaughan/Desktop/Python/sample_data_updated.csv', chunksize=1000):
+  for batch in pd.read_csv('/Users/danielvaughan/Desktop/Python/sample_data.csv', chunksize=1000):
     batchs.append(batch)
   updatedData = pd.concat(batchs, ignore_index=True)
   #Replace NaN with None
   updatedData  = updatedData.where(pd.notnull(updatedData), None)
   #print("Updated", updatedData)
-
 
 def findingNewNetListing():
  
@@ -76,11 +98,11 @@ def findingNewNetListing():
       if propertyId == row.property_id:
         newEntryListing = []
         newEntryListing.append([row.property_id,row.display_address, row.zip_code, row.building_name, row.property_type, row.property_sub_type, row.style, row.acres, row.square_feet, row.year_built, row.total_bedrooms, row.total_bathrooms, row.garage_spaces, row.lat, row.lng, row.area_name, row.city, row.county, row.market_name, row.state, row.original_listing_price, row.listing_price, row.sold_price, row.list_date, row.pending_date, row.sold_date, row.listing_agent_id, row.co_listing_agent_id, row.selling_agent_id, row.co_selling_agent_id, row.listing_office_id, row.co_listing_office_id, row.selling_office_id, row.co_selling_office_id, row.last_update_date])
-        insertingNewNetListing(newEntryListing[0])
+        insertingIntoDestinationTable(newEntryListing[0])
         #print(newEntryListing)
 
-def insertingNewNetListing(newEntryToInsert):
-  #print(newEntryToInsert[25])
+#Inserting new listing or updated listing into destination datatable
+def insertingIntoDestinationTable(newEntryToInsert):
   try:
 
     con = psycopg2.connect(database="source_db_name", user="source_db_user", password="SOURCE_DB_PASSWORD", host="127.0.0.1", port="5432")
@@ -105,12 +127,18 @@ def insertingNewNetListing(newEntryToInsert):
         con.close()
         print("PostgreSQL connection is closed")
 
-def findingTrackedVariableChanged():
-  #Looping over DF inital values in source data and looking at tracked values
-  #sourceTrackedFields = []
-  for row in sourceData.itertuples():
-    sourceTrackedFields = []
+def retreavingPossibleUpdatedListings():
+
+  #Finding all property Id's in new csv file
+  #possibleNewNetListingIds = []
+  for row in updatedData.itertuples():
+
+    newEntryListing = []
+    newEntryListing.append([row.property_id,row.display_address, row.zip_code, row.building_name, row.property_type, row.property_sub_type, row.style, row.acres, row.square_feet, row.year_built, row.total_bedrooms, row.total_bathrooms, row.garage_spaces, row.lat, row.lng, row.area_name, row.city, row.county, row.market_name, row.state, row.original_listing_price, row.listing_price, row.sold_price, row.list_date, row.pending_date, row.sold_date, row.listing_agent_id, row.co_listing_agent_id, row.selling_agent_id, row.co_selling_agent_id, row.listing_office_id, row.co_listing_office_id, row.selling_office_id, row.co_selling_office_id, row.last_update_date])
+          
+    #Finding all property Id's in new csv file
     propertyId = row.property_id
+    #Finding all tracked values
     listingPrice = row.listing_price
     soldPrice = row.sold_price
     listDate = row.list_date
@@ -125,68 +153,29 @@ def findingTrackedVariableChanged():
     sellingOffice = row.selling_office_id
     coSellingOffice = row.co_selling_office_id
 
-    sourceTrackedFields.append([propertyId, listingPrice, soldPrice, listDate, pendingDate, soldDate, listingAgent, coListingAgent, sellingAgent,  coSellingAgent, listingOffice, coListingOffice, sellingOffice, coSellingOffice])  
-    #print(sourceTrackedFields)
-
-
-    for row in updatedData.itertuples():
-      propertyId = row.property_id
-      if sourceTrackedFields[0][0] == propertyId:
-
-        listingPrice = row.listing_price
-        soldPrice = row.sold_price
-        listDate = row.list_date
-        pendingDate = row.pending_date
-        soldDate = row.sold_date
-        listingAgent = row.listing_agent_id
-        coListingAgent = row.co_listing_agent_id
-        sellingAgent = row.selling_agent_id
-        coSellingAgent = row.co_selling_agent_id
-        listingOffice = row.listing_office_id
-        coListingOffice = row.co_listing_office_id
-        sellingOffice = row.selling_office_id
-        coSellingOffice = row.co_selling_office_id
-
-        if sourceTrackedFields[0][1] != listingPrice or sourceTrackedFields[0][2] != soldPrice or sourceTrackedFields[0][3] != listDate or sourceTrackedFields[0][4] != pendingDate or sourceTrackedFields[0][5] != soldDate or sourceTrackedFields[0][6] != listingAgent or sourceTrackedFields[0][7] != coListingAgent or sourceTrackedFields[0][8] != sellingAgent or sourceTrackedFields[0][9] != coSellingAgent or sourceTrackedFields[0][10] != listingOffice or sourceTrackedFields[0][11] != coListingOffice or sourceTrackedFields[0][12] != sellingOffice or sourceTrackedFields[0][13] != coSellingOffice:
-          print("not Matching")
-
-          newEntryListing = []
-          newEntryListing.append([row.property_id,row.display_address, row.zip_code, row.building_name, row.property_type, row.property_sub_type, row.style, row.acres, row.square_feet, row.year_built, row.total_bedrooms, row.total_bathrooms, row.garage_spaces, row.lat, row.lng, row.area_name, row.city, row.county, row.market_name, row.state, row.original_listing_price, row.listing_price, row.sold_price, row.list_date, row.pending_date, row.sold_date, row.listing_agent_id, row.co_listing_agent_id, row.selling_agent_id, row.co_selling_agent_id, row.listing_office_id, row.co_listing_office_id, row.selling_office_id, row.co_selling_office_id, row.last_update_date])
-          insertingNewNetListing(newEntryListing[0])
-
-        #else:
-          #print("Matching")
-        
-      #break
-    #break
-
-def insertingUpdatedDataIntoDestinationTable(newEntryToInsert):
-  try:
-
-    con = psycopg2.connect(database="source_db_name", user="source_db_user", password="SOURCE_DB_PASSWORD", host="127.0.0.1", port="5432")
-    print("Database opened successfully")
-    cur = con.cursor()
-    #cur.execute("INSERT INTO source_table (property_id,display_address,zip_code,building_name,property_type,property_sub_type,style,acres,square_feet,year_built,total_bedrooms,total_bathrooms,garage_spaces,lat,lng,area_name,city,county,market_name,state,original_listing_price,listing_price,sold_price,list_date,pending_date,sold_date,listing_agent_id,co_listing_agent_id,selling_agent_id,co_selling_agent_id,listing_office_id,co_listing_office_id,selling_office_id,co_selling_office_id,last_update_date) VALUES (3420, 'John', 18, 'Computer Science', 'ICT')")
-    postgres_insert_query = """ INSERT INTO destination_table (property_id,display_address,zip_code,building_name,property_type,property_sub_type,style,acres,square_feet,year_built,total_bedrooms,total_bathrooms,garage_spaces,lat,lng,area_name,city,county,market_name,state,original_listing_price,listing_price,sold_price,list_date,pending_date,sold_date,listing_agent_id,co_listing_agent_id,selling_agent_id,co_selling_agent_id,listing_office_id,co_listing_office_id,selling_office_id,co_selling_office_id,last_update_date) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-    record_to_insert = (newEntryToInsert[0], newEntryToInsert[1], newEntryToInsert[2], newEntryToInsert[3], newEntryToInsert[4], newEntryToInsert[5], newEntryToInsert[6], newEntryToInsert[7], newEntryToInsert[8], newEntryToInsert[9], newEntryToInsert[10], newEntryToInsert[11], newEntryToInsert[12], newEntryToInsert[13], newEntryToInsert[14], newEntryToInsert[15], newEntryToInsert[16], newEntryToInsert[17], newEntryToInsert[18], newEntryToInsert[19], newEntryToInsert[20], newEntryToInsert[21], newEntryToInsert[22], newEntryToInsert[23], newEntryToInsert[24], (newEntryToInsert[25]), newEntryToInsert[26], newEntryToInsert[27], newEntryToInsert[28], newEntryToInsert[29], newEntryToInsert[30], newEntryToInsert[31], newEntryToInsert[32], newEntryToInsert[33], newEntryToInsert[34])
-    cur.execute(postgres_insert_query, record_to_insert)
-
-    con.commit()
-    print("Record inserted successfully")
+    try:
+      propertyId = str(propertyId)
+      con = psycopg2.connect(database="source_db_name", user="source_db_user", password="SOURCE_DB_PASSWORD", host="127.0.0.1", port="5432")
+      #print("Database opened successfully")
+      cur = con.cursor()
+      postgreSQL_select_Query = "SELECT listing_price, sold_price, list_date, pending_date, sold_date, listing_agent_id, co_listing_agent_id, selling_agent_id,  co_selling_agent_id, listing_office_id, co_listing_office_id, selling_office_id, co_selling_office_id from source_table WHERE property_id = %s"
   
-  except (Exception, psycopg2.Error) as error :
-    if(con):
-        print("Failed to insert record into table:", error)
+      cur.execute(postgreSQL_select_Query, (propertyId,))
+      updated_records = cur.fetchall()
+      for row in updated_records:
+        if listingPrice != row[0] or soldPrice != row[1] or listDate != row[2] or pendingDate != row[3] or soldDate != row[4] or listingAgent != row[5] or coListingAgent != row[6] or sellingAgent != row[7] or coSellingAgent != row[8] or listingOffice != row[9] or coListingOffice != row[10] or sellingOffice != row[11] or coSellingOffice != row[12]:
+          #print(newEntryListing[0])
+          insertingIntoDestinationTable(newEntryListing[0])
+         
+    except (Exception, psycopg2.Error) as error:
+        print("Error fetching data from PostgreSQL table", error)
 
-  finally:
-    #closing database connection.
-    if(con):
-        #cur.close()
-        con.close()
-        print("PostgreSQL connection is closed")
-
-
-
+    finally:
+        # closing database connection
+        if (con):
+            cur.close()
+            con.close()
+            #print("PostgreSQL connection is closed \n")
 
 def settingUpInitialDataInsert():
    for row in sourceData.itertuples():
@@ -197,7 +186,6 @@ def settingUpInitialDataInsert():
 def insertingInitialDataIntoSourceTable(newEntryToInsert):
  
   try:
-    
     con = psycopg2.connect(database="source_db_name", user="source_db_user", password="SOURCE_DB_PASSWORD", host="127.0.0.1", port="5432")
     #print("Database opened successfully")
     cur = con.cursor()
@@ -340,4 +328,5 @@ def createDestinationTable():
 
 if __name__ == "__main__": 
   main()
+  
   
